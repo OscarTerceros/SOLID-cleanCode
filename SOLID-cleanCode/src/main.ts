6b986c140e564348609c3a73b68d78c3fa69cf80
Oscar Terceros <osterce@gmail.com>
2023-05-03 20:57:40 -0400

    Introduccion

--- SOLID-cleanCode/src/main.ts
+++ SOLID-cleanCode/src/main.ts
@@ -2,10 +2,9 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>CleanCode and SOLID</h1>
-  <span>Revisar la consola de JavaScript</spam>
   <div class='wraper'>
     <h3>Deuda Técnica</h3>
-    <span>La falta de calidad en el código, que genera una deuda que repercutira en costos futuros</spam>
+    <span>La falta de calidad en el código, genera una deuda que repercutirá en costos futuros</spam>
     <h4>Costos económicos</h4>
     <ul>
       <li>Tiempo en realizar matenimiento</li>
@@ -20,6 +19,22 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <li>Prudente -> Tenemos que entregar rápido, ya refactorizaremos</li>
       <li>Inadvertido -> ¿Qué son los patrones de diseño?</li>
       <li>Prudente e Inadvertido -> Ahora sabemos como debíamos haber hecho</li>
+    </ul>
+
+    <h4>¿Cómo se paga la deuda técnica?</h4>
+    <b>Refactorización</b></br>
+    <ul>
+      <li>Es simplemente un proceso que tiene por objetivo mejorar el código sin alterar su comportamiento para que sea más entendible y tolerante a cambios.</li>
+      <li>Usualmente para que una refactorización fuerte tenga el objetivo esperado, es impresindible contar con <b>pruebas automáticas</b>.</li>
+      <li>El no tener pruebas automáticas usualmente lleva al <b>"Si funciona, no lo toques".</b></li>
+      <li>La mala calidad en el software siempre la acaba pagando o asumiendo alguine.</li>
+      <li>Ya sea el cliente, el proveedor con recursos o el propio desarrollador dedicando el tiempo a refactorizar o malgastando el tiempo programando sobre un sistema fragil.</li>
+    </ul>
+    <b>Clean Code</b>
+    <ul>
+      <li>Código limpio es aquel que se ha escrito con la intención de que otra persona (ó tu mismo en el futuro) lo entienda.</li>
+      <li>Nuestro código tiene que ser simple y directo, deberia leerse con la misma facilidad que un texto bien escrito.</li>
+      <li>Programar es el arte de decirle a otro humano lo que quieres que la computadora haga.</li>
     </ul>
   </div>
 `
